e1d8f424c82a8594e7563e9b89f38f57c28ef0b8
Abhiram Danda <abhiramdanda@gmail.com>
2026-04-18 10:54:09 -0500

    Added formatting and opencode plugins

--- init.lua
+++ init.lua
@@ -2,50 +2,56 @@
 require("config.lazy")
 
 -- Setup colorscheme, options, and keymaps
-vim.cmd.colorscheme 'kanagawa-dragon'
+vim.cmd.colorscheme("kanagawa-dragon")
 
 vim.opt.number = true
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4  
-vim.opt.shiftwidth = 4  
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { silent = true, desc = "Toggle Neo-tree" })
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files', { desc = 'Telescope find files' })
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { silent = true, desc = "Toggle Neo-tree" })
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files", { desc = "Telescope find files" })
 
 -- Enable inline error messages
 vim.diagnostic.config({
-  virtual_text = true,   
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  float = {
-    border = "rounded",
-    source = true,
-  },
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	float = {
+		border = "rounded",
+		source = true,
+	},
+})
+
+vim.api.nvim_create_autocmd("QuitPre", {
+	callback = function()
+		vim.cmd("Neotree close")
+	end,
 })
 
 -- Setup LSP servers
 vim.lsp.config("clangd", {
-  cmd = { "clangd", "--background-index", "--clang-tidy" },
-  filetypes = { "c", "cpp" },
-  root_markers = { "compile_commands.json", "CMakeLists.txt", ".git" },
-  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	cmd = { "clangd", "--background-index", "--clang-tidy" },
+	filetypes = { "c", "cpp" },
+	root_markers = { "compile_commands.json", "CMakeLists.txt", ".git" },
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
 vim.lsp.config("basedpyright", {
-  cmd = { "basedpyright-langserver", "--stdio" },
-  filetypes = { "python" },
-  root_markers = { "pyproject.toml", "setup.py", "requirements.txt", ".git" },
-  settings = {
-    basedpyright = {
-      analysis = {
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-        diagnosticMode = "workspace",
-      },
-    },
-  },
-  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	cmd = { "basedpyright-langserver", "--stdio" },
+	filetypes = { "python" },
+	root_markers = { "pyproject.toml", "setup.py", "requirements.txt", ".git" },
+	settings = {
+		basedpyright = {
+			analysis = {
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+				diagnosticMode = "workspace",
+			},
+		},
+	},
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
 vim.lsp.enable("clangd")
--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -1,98 +1,219 @@
 return {
-    {
-       'nvim-treesitter/nvim-treesitter',
-        lazy = false,
-        build = ':TSUpdate'
-    },
-    {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      opts = {
-        preset = "helix", 
-      },
-      keys = {
-        {
-          "<leader>?",
-          function()
-            require("which-key").show({ global = false })
-          end,
-          desc = "Buffer Local Keymaps (which-key)",
-        },
-      },
-    }, 
-    { 
-         "rebelot/kanagawa.nvim",
-    },
-    {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
-    },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        config = function()
-        require("neo-tree").setup({
-            window = {
-                close_if_last_window = true,
-            },
-        })
-        end,
-    },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-          "nvim-lua/plenary.nvim",
-          "MunifTanjim/nui.nvim",
-          "nvim-tree/nvim-web-devicons", -- optional, but recommended
-        },
-        lazy = false, -- neo-tree will lazily load itself
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-          "hrsh7th/cmp-nvim-lsp",
-          "hrsh7th/cmp-buffer",
-          "hrsh7th/cmp-path",
-          "L3MON4D3/LuaSnip",
-          "saadparwaiz1/cmp_luasnip",
-        },
-        config = function()
-          local cmp = require("cmp")
-          local luasnip = require("luasnip")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+	},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+				},
+			})
 
-          cmp.setup({
-            snippet = {
-              expand = function(args)
-                luasnip.lsp_expand(args.body)
-              end,
-            },
-            mapping = cmp.mapping.preset.insert({
-              ["<C-Space>"] = cmp.mapping.complete(),
-              ["<CR>"]      = cmp.mapping.confirm({ select = true }),
-              ["<C-e>"]     = cmp.mapping.abort(),
-              ["<Tab>"]     = cmp.mapping(function(fallback)
-                if cmp.visible() then cmp.select_next_item()
-                elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
-                else fallback() end
-              end, { "i", "s" }),
-              ["<S-Tab>"]   = cmp.mapping(function(fallback)
-                if cmp.visible() then cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then luasnip.jump(-1)
-                else fallback() end
-              end, { "i", "s" }),
-            }),
-            sources = cmp.config.sources({
-              { name = "nvim_lsp" },
-              { name = "luasnip" },
-              { name = "buffer" },
-              { name = "path" },
-            }),
-          })
-        end,
-    },
-    { "github/copilot.vim" }
+			-- Format on demand with <leader>f
+			vim.keymap.set("n", "<leader>f", function()
+				require("conform").format({ async = true })
+			end, { desc = "Format current file" })
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			bigfile = { enabled = true },
+			dashboard = { enabled = true },
+			indent = { enabled = true },
+			input = { enabled = true },
+			picker = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+		},
+	},
+	{
+		"nickjvandyke/opencode.nvim",
+		version = "*", -- Latest stable release
+		dependencies = {
+			{
+				-- `snacks.nvim` integration is recommended, but optional
+				---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+				"folke/snacks.nvim",
+				optional = true,
+				opts = {
+					input = {}, -- Enhances `ask()`
+					picker = { -- Enhances `select()`
+						actions = {
+							opencode_send = function(...)
+								return require("opencode").snacks_picker_send(...)
+							end,
+						},
+						win = {
+							input = {
+								keys = {
+									["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		config = function()
+			---@type opencode.Opts
+			vim.g.opencode_opts = {
+				-- Your configuration, if any; goto definition on the type or field for details
+			}
+
+			vim.o.autoread = true -- Required for `opts.events.reload`
+
+			-- Recommended/example keymaps
+			vim.keymap.set({ "n", "x" }, "<leader>a", function()
+				require("opencode").ask("@this: ", { submit = true })
+			end, { desc = "Ask opencode…" })
+			vim.keymap.set({ "n", "x" }, "<leader>x", function()
+				require("opencode").select()
+			end, { desc = "Execute opencode action…" })
+			vim.keymap.set({ "n", "t" }, "<leader>o", function()
+				require("opencode").toggle()
+			end, { desc = "Toggle opencode" })
+
+			vim.keymap.set({ "n", "x" }, "go", function()
+				return require("opencode").operator("@this ")
+			end, { desc = "Add range to opencode", expr = true })
+			vim.keymap.set("n", "goo", function()
+				return require("opencode").operator("@this ") .. "_"
+			end, { desc = "Add line to opencode", expr = true })
+
+			vim.keymap.set("n", "<S-C-u>", function()
+				require("opencode").command("session.half.page.up")
+			end, { desc = "Scroll opencode up" })
+			vim.keymap.set("n", "<S-C-d>", function()
+				require("opencode").command("session.half.page.down")
+			end, { desc = "Scroll opencode down" })
+
+			-- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
+			vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+			vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			preset = "helix",
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		"rebelot/kanagawa.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- optional but recommended
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		config = function()
+			require("neo-tree").setup({
+				event_handlers = {
+					{
+						event = "neo_tree_buffer_leave",
+						handler = function() end,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false, -- neo-tree will lazily load itself
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
+		end,
+	},
+	{ "github/copilot.vim" },
 }
